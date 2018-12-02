1216f0a50a334037a49740af82f37e8d31e2c177
rthakk20 <rthakk20illinois.edu>
2018-12-01 19:14:03 -0600

    UI Mock Up is almost done

--- app/src/main/java/com/example/rthakk20/etchasketch/MainActivity.java
+++ app/src/main/java/com/example/rthakk20/etchasketch/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rthakk20.etchasketch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Left(android.view.View left) {
+        Toast.makeText(this, "You pressed the Left button", Toast.LENGTH_SHORT).show();
+    }
+    public void Right(android.view.View right) {
+        Toast.makeText(this, "You pressed the Right button", Toast.LENGTH_SHORT).show();
+    }
+    public void Up(android.view.View up) {
+        Toast.makeText(this, "You pressed the Up button", Toast.LENGTH_SHORT).show();
+    }
+    public void Down(android.view.View down) {
+        Toast.makeText(this, "You pressed the Down button", Toast.LENGTH_SHORT).show();
     }
 }
