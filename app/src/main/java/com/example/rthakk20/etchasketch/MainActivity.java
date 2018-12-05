747749ed424b9c91e497c93d229118729749bf42
sohailkhan <sohailk2@illinois.edu>
2018-12-05 13:58:27 -0600

    removed notifcation bar so the app is fullscreen

--- app/src/main/java/com/example/rthakk20/etchasketch/MainActivity.java
+++ app/src/main/java/com/example/rthakk20/etchasketch/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
